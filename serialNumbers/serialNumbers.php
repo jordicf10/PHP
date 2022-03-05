3213a10c56ebde8c9d440d08c2818d286c128819
= <=>
2022-03-05 19:30:37 +0100

    Second version of serial number, asking for input data

--- serialNumbers/serialNumbers.php
+++ serialNumbers/serialNumbers.php
@@ -1,27 +1,35 @@
 <?php
 
 function series_sum($n) {
-  $n = round($n);
-  $num1= 1;
+  $n = round((string)$n);
+  $num1= "1";
   echo "Series: ";
   echo $num1;
   $divisor = $num1;
-  $sum= 1;
+  $sum= "1";
   
-  for($i=1;$i<=$n;$i++){
+  for($i=1;$i<$n;$i++){
     
     echo " + {$num1}/{$divisor}";
     $sum+= $num1/$divisor;
-    $divisor+= 3;
+    $sum = (string)$sum;
+    $divisor+= "3";
+    $divisor = (string)$divisor;
   }
   
   $total=(string)$sum;
     if($total=="0"){
      echo number_format($total, 2);
     }
-    $totall = round($total,2);
+    $est = round($total,2);
+    $totall = (string)$est;
     echo "\n";
     echo $totall;
 }
 
-echo series_sum(4);
+$serial = readline("How many long do you want to give to your serial number:");
+$result = (string)series_sum($serial);
+echo $result;
+$tipo = gettype($result);
+echo "\n";
+echo $tipo;
